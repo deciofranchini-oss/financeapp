ccde4ee4a4cb74b6e632ae22ff8df045d87977f7
Décio <deciofranchini@gmail.com>
2026-02-28 11:21:46 -0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const SUPABASE_URL = "https://rrdxukcfdpacnixqhgsp.supabase.co";
+// Cole aqui sua anon public key (Project Settings -> API -> anon public)
 export const SUPABASE_ANON_KEY = "sb_publishable_7r4DiXpMK9KroZ-szGADEA_LkpfWhx0";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
