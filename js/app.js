599a2f4ebe24b5c08c665841d1588b2f79f41770
Décio <deciofranchini@gmail.com>
2026-03-16 12:32:41 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -436,7 +436,7 @@ function getAppBaseUrl() {
   return origin + base;
 }
 
-const DEFAULT_LOGO_URL='https://deciofranchini-oss.github.io/fintrack/logo.png';
+const DEFAULT_LOGO_URL='logo_transparent.png';
 let APP_LOGO_URL=DEFAULT_LOGO_URL;
 function setAppLogo(url){
   // Defensive: avoid accidentally assigning a Promise/thenable to img.src
